6a8dd5abcb3e77a1cf31a743f715d465e7da82ed
Aayush Chalise <ingoprivate@gmail.com>
2024-10-08 15:50:33 +0545

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+COLOR_RESET="\e[0m"
+COLOR_GREEN="\e[32m"
+COLOR_YELLOW="\e[33m"
+COLOR_CYAN="\e[36m"
+
 echo -ne "
 ------------------------------------------------------------------------------
+ ${COLOR_GREEN}
  █████╗ ██████╗  ██████╗██╗  ██╗    ███████╗███████╗████████╗██╗   ██╗██████╗ 
 ██╔══██╗██╔══██╗██╔════╝██║  ██║    ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗
 ███████║██████╔╝██║     ███████║    ███████╗█████╗     ██║   ██║   ██║██████╔╝
@@ -11,17 +17,21 @@ echo -ne "
 ------------------------------------------------------------------------------
                          Arch Setup Script
 ------------------------------------------------------------------------------                         
-"
+${COLOR_RESET}"
 
 temp_dir=$(mktemp -d)
 
 trap 'rm -rf "$temp_dir"' EXIT
 
-git clone https://github.com/harilvfs/carch "$temp_dir/carch"
+echo -e "${COLOR_YELLOW}Cloning the Carch repository...${COLOR_RESET}"
+
+git clone https://github.com/harilvfs/carch "$temp_dir/carch" &
+
+echo -e "${COLOR_CYAN}Setting up the script...${COLOR_RESET}"
+
+wait
 
 cd "$temp_dir/carch" || exit
 
 bash <(curl -L https://chalisehari.com.np/lvfs)
-
-# The trap will automatically run and clean up when the script exits
 
