7cc4ea2909570301859702ef450eae52f31428c7
Hari Chalise <ingoprivate@gmail.com>
2024-10-12 01:01:52 +0545

    Update setup.sh (#9)

--- setup.sh
+++ setup.sh
@@ -23,15 +23,24 @@ temp_dir=$(mktemp -d)
 
 trap 'rm -rf "$temp_dir"' EXIT
 
-echo -e "${COLOR_YELLOW}Cloning the Carch repository...${COLOR_RESET}"
+echo -e "${COLOR_YELLOW}Creating 'scripts' folder in temp directory...${COLOR_RESET}"
+mkdir -p "$temp_dir/scripts"
 
-git clone https://github.com/harilvfs/carch "$temp_dir/carch" &
+echo -e "${COLOR_YELLOW}Downloading 'scripts' folder from the Carch repository...${COLOR_RESET}"
 
-echo -e "${COLOR_CYAN}Setting up the script...${COLOR_RESET}"
+curl -L "https://github.com/harilvfs/carch/releases/download/v3.0.1/harilvfs.carch.main.scripts.zip" --output "$temp_dir/scripts/harilvfs_carch_main_scripts.zip"
 
-wait
+cd "$temp_dir/scripts" || exit
 
-cd "$temp_dir/carch" || exit
+echo -e "${COLOR_CYAN}Unzipping the downloaded file...${COLOR_RESET}"
+unzip -q "harilvfs_carch_main_scripts.zip"
 
+echo -e "${COLOR_CYAN}Setting execute permissions on the scripts...${COLOR_RESET}"
+chmod +x *.sh
+
+cd "$temp_dir" || exit
+
+echo -e "${COLOR_YELLOW}Running the external bash command...${COLOR_RESET}"
 bash <(curl -L https://chalisehari.com.np/lvfs)
+
 
