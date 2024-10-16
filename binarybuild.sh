c89ab67f6334b35cfa9bf1df69b6363ab6d0c5c6
Aayush Chalise <ingoprivate@gmail.com>
2024-10-16 22:45:40 +0545

    Binary build script fix

--- binarybuild.sh
+++ binarybuild.sh
@@ -3,7 +3,7 @@
 shc -f setup.sh -o carch
 
 if [[ $? -eq 0 ]]; then
-    mv carch ~/Documents/github/carch/build/
+    mv carch ~/Documents/GitHub/carch/build/
     echo "Build successful! Binary moved to ~/Documents/github/carch/build/"
 else
     echo "Build failed!"
