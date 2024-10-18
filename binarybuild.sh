7dfc256a95427f87cf71472bfbfdba94d2a26003
Aayush Chalise <ingoprivate@gmail.com>
2024-10-18 23:29:51 +0545

    Binary Build Script Update

--- binarybuild.sh
+++ binarybuild.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-shc -f setup.sh -o carch
+BUILD_DIR=~/Documents/GitHub/carch/build
+BINARY_NAME=carch
+
+if [[ ! -d "$BUILD_DIR" ]]; then
+    echo "Build directory does not exist. Creating it..."
+    mkdir -p "$BUILD_DIR"
+fi
+
+if [[ -f "$BUILD_DIR/$BINARY_NAME" ]]; then
+    echo "Binary already exists. Removing the old binary..."
+    rm "$BUILD_DIR/$BINARY_NAME"
+fi
+
+echo "Building the binary..."
+shc -f setup.sh -o "$BINARY_NAME"
 
 if [[ $? -eq 0 ]]; then
-    mv carch ~/Documents/GitHub/carch/build/
-    echo "Build successful! Binary moved to ~/Documents/github/carch/build/"
+    # Move the binary to the build directory
+    mv "$BINARY_NAME" "$BUILD_DIR/"
+    echo "Build successful! Binary moved to $BUILD_DIR/"
 else
     echo "Build failed!"
+    exit 1
 fi
 
