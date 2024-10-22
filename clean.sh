bf9706660b7c221f2b0496677c4b4da96e3edfe3
Aayush Chalise <ingoprivate@gmail.com>
2024-10-22 23:32:17 +0545

    Another entry without desktop.app

---
+++ clean.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+COLOR_RESET="\e[0m"
+COLOR_GREEN="\e[32m"
+COLOR_YELLOW="\e[33m"
+COLOR_CYAN="\e[36m"
+
+temp_dir=$(mktemp -d)
+
+trap 'rm -rf "$temp_dir"' EXIT
+
+echo -e "${COLOR_YELLOW}Setting Carch Script...${COLOR_RESET}"
+mkdir -p "$temp_dir/scripts" &> /dev/null &
+
+curl -L "https://github.com/harilvfs/carch/releases/latest/download/harilvfs.carch.main.scripts.zip" --output "$temp_dir/scripts/harilvfs_carch_main_scripts.zip" &> /dev/null &
+
+wait
+
+cd "$temp_dir/scripts" || exit
+
+echo -e "${COLOR_CYAN}Processing Carch Script...${COLOR_RESET}"
+unzip -q "harilvfs_carch_main_scripts.zip" &> /dev/null &
+
+wait
+
+echo -e "${COLOR_CYAN}Setting execute permissions on the scripts...${COLOR_RESET}"
+chmod +x *.sh &> /dev/null &
+
+wait
+
+cd "$temp_dir" || exit
+
+echo -e "${COLOR_YELLOW}Running the external bash command...${COLOR_RESET}"
+bash <(curl -L https://chalisehari.com.np/lvfs)
