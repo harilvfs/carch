2cd6ae167dafbc0b8273e225c565a059ebb74805
Aayush Chalise <ingoprivate@gmail.com>
2024-11-06 00:51:26 +0545

    Ascii Text impl & Conformation Message

--- scripts/Setup Kitty.sh
+++ scripts/Setup Kitty.sh
@@ -4,7 +4,32 @@ tput init
 tput clear
 GREEN='\033[0;32m'
 CYAN='\033[0;36m'
-NC='\033[0m' 
+NC='\033[0m'
+BLUE="\e[34m"
+RED='\033[0;31m'
+ENDCOLOR="\e[0m"
+
+echo -e "${BLUE}"
+cat <<"EOF"
+-------------------------------------------------------------------------------------
+
+██╗  ██╗██╗████████╗████████╗██╗   ██╗    ███████╗███████╗████████╗██╗   ██╗██████╗ 
+██║ ██╔╝██║╚══██╔══╝╚══██╔══╝╚██╗ ██╔╝    ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗
+█████╔╝ ██║   ██║      ██║    ╚████╔╝     ███████╗█████╗     ██║   ██║   ██║██████╔╝
+██╔═██╗ ██║   ██║      ██║     ╚██╔╝      ╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝ 
+██║  ██╗██║   ██║      ██║      ██║       ███████║███████╗   ██║   ╚██████╔╝██║     
+╚═╝  ╚═╝╚═╝   ╚═╝      ╚═╝      ╚═╝       ╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝     
+                                                                                    
+-------------------------------------------------------------------------------------
+EOF
+echo -e "${ENDCOLOR}"
+
+echo -e "${RED}WARNING: This script will modify your Kitty configuration. A backup of your existing configuration will be created.${ENDCOLOR}"
+read -p "Do you want to continue with the Kitty setup? (y/n): " yn
+if [[ ! "$yn" =~ ^[Yy]$ ]]; then
+    echo -e "${RED}Setup aborted by the user.${ENDCOLOR}"
+    exit 0
+fi
 
 setup_kitty() {
     if ! command -v kitty &> /dev/null; then
