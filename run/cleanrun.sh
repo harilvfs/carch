61d08dea71dc090fddaa7f1e3fbcba916961c51c
Hari Chalise <ingoprivate@gmail.com>
2024-11-09 21:47:15 +0545

    Replace Whiptail and Dialog with Gum and Figlet for Simplified Menu (#19)
    * Update cxfs.sh
    
    * Menu Changes
    
    * Add gim & figlet dependencies installation
    
    * Update run.sh
    
    * Clean Run Script [ For /usr/bin/carch ] Binary
    
    * Clean Run Script [ For /usr/bin/carch ] Binary
    
    * Redirect Link Tho Cloudflare
    
    * Carch Preview GIF
    
    * Refactor & Remove html crepp
    
    * Co-authored-by: Hari Chalise <harilvfs@chalisehari.com.np>
    
    * Build Binary
    
    Co-authored-by: Hari Chalise <harilvfs@chalisehari.com.np>

--- run/cleanrun.sh
+++ run/cleanrun.sh
@@ -3,27 +3,6 @@
 COLOR_CYAN="\e[36m"
 COLOR_RESET="\e[0m"
 
-install_if_missing() {
-    local package_name="$1"
-    local install_cmd="$2"
-    local check_cmd="$3"
-
-    if ! command -v "$check_cmd" &> /dev/null; then
-        echo "$package_name is not installed. Installing..."
-        sudo $install_cmd
-        if [ $? -ne 0 ]; then
-            echo "Failed to install $package_name."
-            exit 1
-        fi
-    else
-        echo "$package_name is already installed. Skipping installation."
-    fi
-}
-
-install_if_missing "libnewt" "pacman -S --noconfirm libnewt" "whiptail"
-install_if_missing "gum" "pacman -S --noconfirm gum" "gum"
-install_if_missing "figlet" "pacman -S --noconfirm figlet" "figlet"
-
 REPO="harilvfs/carch"
 BINARY_NAME="cxfs.sh"
 TEMP_FILE=$(mktemp /tmp/$BINARY_NAME.XXXXXX)
@@ -58,3 +37,4 @@ chmod +x "$TEMP_FILE"
 rm -f "$TEMP_FILE"
 
 echo -e "${COLOR_CYAN}See You...${COLOR_RESET}"
+
