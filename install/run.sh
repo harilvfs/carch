cd35ddaa8d0f02b007fbb792a6e25aeb25b8a24a
Hari Chalise <ingoprivate@gmail.com>
2024-12-24 22:51:19 +0545

    Fix Dependencies Installation
    Co-Authored-By: aayush <177418358+aayushrg7@users.noreply.github.com>
    Co-Authored-By: Smriti Bhandari <187972284+smritics@users.noreply.github.com>

--- install/run.sh
+++ install/run.sh
@@ -36,6 +36,33 @@ else
     echo "gtk3 is already installed. Skipping installation."
 fi
 
+if ! pacman -Q noto-fonts-emoji &>/dev/null; then
+    echo "note-fonts-emoji is not installed. Installing..."
+    sudo pacman -S --noconfirm noto-fonts-emoji
+    if [ $? -ne 0 ]; then
+        echo "Failed to install noto-fonts-emoji."
+        exit 1
+    fi
+else
+    echo "noto-fonts-emoji is already installed. Skipping installation."
+fi
+
+if ! pacman -Q ttf-joypixels &>/dev/null; then
+    echo "ttf-joypixels is not installed. Installing..."
+    sudo pacman -S --noconfirm ttf-joypixels 
+    if [ $? -ne 0 ]; then
+        echo "Failed to install ttf-joypixels."
+        exit 1
+    fi
+else
+    echo "ttf-joypixels is already installed. Skipping installation."
+fi
+
+echo -e "${COLOR_YELLOW}Running the external bash command...${COLOR_RESET}"
+curl -fsSL https://chalisehari.com.np/carch | sh 
+
+figlet -f slant Note
+
 echo -e "${COLOR_CYAN}Carch has been successfully installed!${COLOR_RESET}"
 echo -e "${COLOR_CYAN}Use 'carch' or 'carch-gtk' to run the Carch script.${COLOR_RESET}"
 echo -e "${COLOR_CYAN}For available commands, type 'carchcli --help'.${COLOR_RESET}"
