ab6a934d42f7722ba3d5e5a7d618d18d16be0d6e
Hari Chalise <ingoprivate@gmail.com>
2024-12-27 22:49:24 +0545

    Making Script Clean 🧹
    Co-Authored-By: aayush <177418358+aayushrg7@users.noreply.github.com>
    Co-Authored-By: Smriti Bhandari <187972284+smritics@users.noreply.github.com>

--- install/run.sh
+++ install/run.sh
@@ -12,7 +12,7 @@ install_if_missing() {
 
     if ! command -v "$check_cmd" &> /dev/null; then
         echo -e "${COLOR_YELLOW}$package_name is not installed. :: Installing...${COLOR_RESET}"
-        sudo $install_cmd
+        sudo $install_cmd &>/dev/null
         if [ $? -ne 0 ]; then
             echo -e "${COLOR_RED}Failed to install $package_name.${COLOR_RESET}"
             exit 1
@@ -26,63 +26,29 @@ install_if_missing "gum" "pacman -S --noconfirm gum" "gum"
 install_if_missing "figlet" "pacman -S --noconfirm figlet" "figlet"
 install_if_missing "Python" "pacman -S --noconfirm python" "python3"
 
-if ! pacman -Q gtk3 &>/dev/null; then
-    echo -e "${COLOR_YELLOW}gtk3 is not installed. :: Installing...${COLOR_RESET}"
-    sudo pacman -S --noconfirm gtk3
-    if [ $? -ne 0 ]; then
-        echo -e "${COLOR_RED}Failed to install gtk3.${COLOR_RESET}"
-        exit 1
+install_package() {
+    local package_name="$1"
+    if ! pacman -Q "$package_name" &>/dev/null; then
+        echo -e "${COLOR_YELLOW}$package_name is not installed. :: Installing...${COLOR_RESET}"
+        sudo pacman -S --noconfirm "$package_name" &>/dev/null
+        if [ $? -ne 0 ]; then
+            echo -e "${COLOR_RED}Failed to install $package_name.${COLOR_RESET}"
+            exit 1
+        fi
+    else
+        echo -e "${COLOR_GREEN}$package_name is already installed. Skipping installation.${COLOR_RESET}"
     fi
-else
-    echo -e "${COLOR_GREEN}gtk3 is already installed. Skipping installation.${COLOR_RESET}"
-fi
+}
 
-if ! pacman -Q noto-fonts-emoji &>/dev/null; then
-    echo -e "${COLOR_YELLOW}noto-fonts-emoji is not installed. :: Installing...${COLOR_RESET}"
-    sudo pacman -S --noconfirm noto-fonts-emoji
-    if [ $? -ne 0 ]; then
-        echo -e "${COLOR_RED}Failed to install noto-fonts-emoji.${COLOR_RESET}"
-        exit 1
-    fi
-else
-    echo -e "${COLOR_GREEN}noto-fonts-emoji is already installed. Skipping installation.${COLOR_RESET}"
-fi
-
-if ! pacman -Q ttf-joypixels &>/dev/null; then
-    echo -e "${COLOR_YELLOW}ttf-joypixels is not installed. :: Installing...${COLOR_RESET}"
-    sudo pacman -S --noconfirm ttf-joypixels
-    if [ $? -ne 0 ]; then
-        echo -e "${COLOR_RED}Failed to install ttf-joypixels.${COLOR_RESET}"
-        exit 1
-    fi
-else
-    echo -e "${COLOR_GREEN}ttf-joypixels is already installed. Skipping installation.${COLOR_RESET}"
-fi
-
-if ! pacman -Q man-pages &>/dev/null; then
-    echo -e "${COLOR_YELLOW}man-pages is not installed. :: Installing...${COLOR_RESET}"
-    sudo pacman -S --noconfirm man-pages
-    if [ $? -ne 0 ]; then
-        echo -e "${COLOR_RED}Failed to install man-pages.${COLOR_RESET}"
-        exit 1
-    fi
-else
-    echo -e "${COLOR_GREEN}man-pages is already installed. Skipping installation.${COLOR_RESET}"
-fi
-
-if ! pacman -Q man-db &>/dev/null; then
-    echo -e "${COLOR_YELLOW}man-db is not installed. :: Installing...${COLOR_RESET}"
-    sudo pacman -S --noconfirm man-db
-    if [ $? -ne 0 ]; then
-        echo -e "${COLOR_RED}Failed to install man-db.${COLOR_RESET}"
-        exit 1
-    fi
-else
-    echo -e "${COLOR_GREEN}man-db is already installed. Skipping installation.${COLOR_RESET}"
-fi
+install_package "gtk3"
+install_package "noto-fonts-emoji"
+install_package "ttf-joypixels"
+install_package "man-pages"
+install_package "man-db"
 
 echo -e "${COLOR_YELLOW}:: Running the external bash command...${COLOR_RESET}"
 if ! bash <(curl -L https://chalisehari.com.np/carch); then
     echo -e "${COLOR_RED}Failed to execute the external bash command.${COLOR_RESET}"
     exit 1
 fi
+
