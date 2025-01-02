9592aeb7b81be3cb3045bcea56cbd74b33c5cd5d
Hari Chalise <ingoprivate@gmail.com>
2025-01-02 12:45:52 +0545

    Fix
    Co-Authored-By: aayush <177418358+aayushrg7@users.noreply.github.com>

--- install/tui.sh
+++ install/tui.sh
@@ -6,27 +6,27 @@ RC='\033[0m'
 RED='\033[0;31m'
 
 get_latest_release() {
-  latest_release=$(curl -s https://api.github.com/repos/harilvfs/carch/releases | 
-    grep -oP '"tag_name": "\K[^"]*' | 
-    head -n 1)
-  if [ -z "$latest_release" ]; then
-    printf "%b\n" "Error fetching release data" >&2
-    return 1
-  fi
-  printf "%b\n" "$latest_release"
+    latest_release=$(curl -s https://api.github.com/repos/harilvfs/carch/releases |
+	        grep -oP '"tag_name": "\K[^"]*' |
+	        head -n 1)
+	    if [ -z "$latest_release" ]; then
+	        printf "%b\n" "Error fetching release data" >&2
+	        return 1
+	    fi
+	    printf "%b\n" "$latest_release"
 }
 
 redirect_to_latest_pre_release() {
-  latest_release=$(get_latest_release)
-  if [ -n "$latest_release" ]; then
-    url="https://github.com/harilvfs/carch/releases/download/$latest_release/carch-tui"
-  else
-    printf "%b\n" 'Unable to determine latest pre-release version.' >&2
-    printf "%b\n" "Using latest Full Release"
-    url="https://github.com/harilvfs/carch/releases/latest/download/carch-tui"
-  fi
-  addArch
-  printf "%b\n" "Using URL: $url"
+    latest_release=$(get_latest_release)
+	    if [ -n "$latest_release" ]; then
+	        url="https://github.com/harilvfs/carch/releases/download/$latest_release/carch-tui"
+	    else
+	        printf "%b\n" 'Unable to determine latest pre-release version.' >&2
+	        printf "%b\n" "Using latest Full Release"
+	        url="https://github.com/harilvfs/carch/releases/latest/download/carch-tui"
+	    fi
+	    addArch
+	    printf "%b\n" "Using URL: $url"
 }
 
 check() {
@@ -41,15 +41,15 @@ check() {
 
 addArch() {
     case "${arch}" in
-        x86_64);;
-        *) url="${url}-${arch}";;
+        x86_64) ;;
+	        *) url="${url}-${arch}" ;;
     esac
 }
 
 findArch() {
     case "$(uname -m)" in
-        x86_64|amd64) arch="x86_64" ;;
-        *) check 1 "Unsupported architecture"
+        x86_64 | amd64) arch="x86_64" ;;
+	        *) check 1 "Unsupported architecture" ;;
     esac
 }
 
