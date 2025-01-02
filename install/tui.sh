f23d4171d6097f324ba3fda0687030da1d3ccba1
Hari Chalise <ingoprivate@gmail.com>
2025-01-02 12:49:18 +0545

    Update tui.sh
    Co-Authored-By: aayush <177418358+aayushrg7@users.noreply.github.com>

--- install/tui.sh
+++ install/tui.sh
@@ -7,26 +7,26 @@ RED='\033[0;31m'
 
 get_latest_release() {
     latest_release=$(curl -s https://api.github.com/repos/harilvfs/carch/releases |
-	        grep -oP '"tag_name": "\K[^"]*' |
-	        head -n 1)
-	    if [ -z "$latest_release" ]; then
-	        printf "%b\n" "Error fetching release data" >&2
-	        return 1
-	    fi
-	    printf "%b\n" "$latest_release"
+        grep -oP '"tag_name": "\K[^"]*' |
+        head -n 1)
+    if [ -z "$latest_release" ]; then
+        printf "%b\n" "Error fetching release data" >&2
+        return 1
+    fi
+    printf "%b\n" "$latest_release"
 }
 
 redirect_to_latest_pre_release() {
     latest_release=$(get_latest_release)
-	    if [ -n "$latest_release" ]; then
-	        url="https://github.com/harilvfs/carch/releases/download/$latest_release/carch-tui"
-	    else
-	        printf "%b\n" 'Unable to determine latest pre-release version.' >&2
-	        printf "%b\n" "Using latest Full Release"
-	        url="https://github.com/harilvfs/carch/releases/latest/download/carch-tui"
-	    fi
-	    addArch
-	    printf "%b\n" "Using URL: $url"
+    if [ -n "$latest_release" ]; then
+        url="https://github.com/harilvfs/carch/releases/download/$latest_release/carch-tui"
+    else
+        printf "%b\n" 'Unable to determine latest pre-release version.' >&2
+        printf "%b\n" "Using latest Full Release"
+        url="https://github.com/harilvfs/carch/releases/latest/download/carch-tui"
+    fi
+    addArch
+    printf "%b\n" "Using URL: $url"
 }
 
 check() {
@@ -42,14 +42,14 @@ check() {
 addArch() {
     case "${arch}" in
         x86_64) ;;
-	        *) url="${url}-${arch}" ;;
+        *) url="${url}-${arch}" ;;
     esac
 }
 
 findArch() {
     case "$(uname -m)" in
         x86_64 | amd64) arch="x86_64" ;;
-	        *) check 1 "Unsupported architecture" ;;
+        *) check 1 "Unsupported architecture" ;;
     esac
 }
 
@@ -71,3 +71,4 @@ check $? "Executing carch-tui"
 
 rm -f "$TMPFILE"
 check $? "Deleting the temporary file"
+
