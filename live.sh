6b99efccb7fa9a208f51704002fe87a888d5b23d
Hari Chalise <ingoprivate@gmail.com>
2025-03-09 12:24:32 +0545

    Creating Live Testing [ Carch ]
    Co-Authored-By: aayush <177418358+aayushrg7@users.noreply.github.com>
    Co-Authored-By: aayushxvf <147874416+aayushxvf@users.noreply.github.com>
    Co-Authored-By: Smriti Bhandari <187972284+smritics@users.noreply.github.com>

---
+++ live.sh
@@ -0,0 +1,52 @@
+#!/bin/sh -e
+
+# Source: https://github.com/ChrisTitusTech/linutil/blob/main/start.sh
+
+{
+rc='\033[0m'
+red='\033[0;31m'
+
+check() {
+    exit_code=$1
+    message=$2
+
+    if [ "$exit_code" -ne 0 ]; then
+        printf '%sERROR: %s%s\n' "$red" "$message" "$rc"
+        exit 1
+    fi
+
+    unset exit_code
+    unset message
+}
+
+findArch() {
+    case "$(uname -m)" in
+        x86_64|amd64) arch="x86_64" ;;
+        aarch64|arm64) arch="aarch64" ;;
+        *) check 1 "Unsupported architecture"
+    esac
+}
+
+getUrl() {
+    case "${arch}" in
+        x86_64) echo "https://github.com/harilvfs/carch/releases/latest/download/carch";;
+        *) echo "https://github.com/harilvfs/carch/releases/latest/download/carch-${arch}";;
+    esac
+}
+
+findArch
+temp_file=$(mktemp)
+check $? "Creating the temporary file"
+
+curl -fsL "$(getUrl)" -o "$temp_file"
+check $? "Downloading carch"
+
+chmod +x "$temp_file"
+check $? "Making carch executable"
+
+"$temp_file" "$@"
+check $? "Executing carch"
+
+rm -f "$temp_file"
+check $? "Deleting the temporary file"
+} 
