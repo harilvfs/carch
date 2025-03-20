f949ffb21294ce80b636c05d37610832ea9de423
Hari Chalise <ingoprivate@gmail.com>
2025-03-20 20:59:29 +0545

    Some Enhancement Check UP (#331)
    Co-authored-by: aayush <177418358+aayushrg7@users.noreply.github.com>

--- live.sh
+++ live.sh
@@ -1,23 +1,32 @@
 #!/bin/sh -e
 
-# Source: https://github.com/ChrisTitusTech/linutil/blob/main/start.sh
-# Make sure to have the gum, figlet, and fzf packages already installed.
-
-{
 rc='\033[0m'
 red='\033[0;31m'
+
+check_dependency() {
+    if ! command -v "$1" >/dev/null 2>&1; then
+        if command -v pacman >/dev/null 2>&1; then
+            sudo pacman -Sy --noconfirm "$1"
+        elif command -v dnf >/dev/null 2>&1; then
+            sudo dnf install -y "$1"
+        else
+            printf '%sERROR: Package manager not supported%s\n' "$red" "$rc"
+            exit 1
+        fi
+    fi
+}
+
+for pkg in fzf gum figlet; do
+    check_dependency "$pkg"
+done
 
 check() {
     exit_code=$1
     message=$2
-
     if [ "$exit_code" -ne 0 ]; then
         printf '%sERROR: %s%s\n' "$red" "$message" "$rc"
         exit 1
     fi
-
-    unset exit_code
-    unset message
 }
 
 findArch() {
@@ -30,24 +39,35 @@ findArch() {
 
 getUrl() {
     case "${arch}" in
-        x86_64) echo "https://github.com/harilvfs/carch/releases/latest/download/carch";;
-        *) echo "https://github.com/harilvfs/carch/releases/latest/download/carch-${arch}";;
+        x86_64) echo "https://github.com/harilvfs/carch/releases/latest/download/carch" ;;
+        *) echo "https://github.com/harilvfs/carch/releases/latest/download/carch-${arch}" ;;
     esac
+}
+
+spinner() {
+    local pid=$1
+    local spin="⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"
+    local i=0
+
+    while kill -0 "$pid" 2>/dev/null; do
+        printf "\r%s" "${spin:i++%${#spin}:1}"
+        sleep 0.1
+    done
+    printf "\r✔\n"
 }
 
 findArch
 temp_file=$(mktemp)
 check $? "Creating the temporary file"
 
-curl -fsL "$(getUrl)" -o "$temp_file"
-check $? "Downloading carch"
+curl -fsL "$(getUrl)" -o "$temp_file" &  
+spinner $!
 
+check $? "Downloading carch"
 chmod +x "$temp_file"
 check $? "Making carch executable"
-
 "$temp_file" "$@"
 check $? "Executing carch"
-
 rm -f "$temp_file"
 check $? "Deleting the temporary file"
-} 
+
