f45b1c9aafc3c73fc46e4baef4fd013b88ff12d7
Hari Chalise <ingoprivate@gmail.com>
2025-04-11 01:04:17 +0545

    remove: Cleanup

--- completions/fish/carch.fish
+++ completions/fish/carch.fish
@@ -1,7 +1,7 @@
 function __fish_carch_no_subcommand
     for i in (commandline -opc)
         switch $i
-            case --help --version --gen-config --config -c --run-script -r --list-scripts -l --search -s --no-preview --disable-preview --log --enable-logging --check-update --update --uninstall
+            case --help -h --version -v --gen-config -c --run-script -r --list-scripts -l --no-preview --log --check-update --update --uninstall
                 return 1
         end
     end
@@ -9,20 +9,17 @@ function __fish_carch_no_subcommand
 end
 
 complete -c carch -n '__fish_carch_no_subcommand' -l help -d "Show the help message"
+complete -c carch -n '__fish_carch_no_subcommand' -s h -d "Show the help message"
 complete -c carch -n '__fish_carch_no_subcommand' -l version -d "Show the program version"
+complete -c carch -n '__fish_carch_no_subcommand' -s v -d "Show the program version"
 complete -c carch -n '__fish_carch_no_subcommand' -l gen-config -d "Generate a default configuration file"
-complete -c carch -n '__fish_carch_no_subcommand' -l config -d "Provide a path to the configuration file" -r
 complete -c carch -n '__fish_carch_no_subcommand' -s c -d "Use the default configuration file"
 complete -c carch -n '__fish_carch_no_subcommand' -l run-script -d "Run a specific script from the script directory" -r
 complete -c carch -n '__fish_carch_no_subcommand' -s r -d "Run a specific script from the script directory" -r
 complete -c carch -n '__fish_carch_no_subcommand' -l list-scripts -d "List all available scripts"
 complete -c carch -n '__fish_carch_no_subcommand' -s l -d "List all available scripts"
-complete -c carch -n '__fish_carch_no_subcommand' -l search -d "Search for scripts by keyword"
-complete -c carch -n '__fish_carch_no_subcommand' -l no-preview -d "Run without displaying script previews in menus (one-time)"
-complete -c carch -n '__fish_carch_no_subcommand' -l disable-preview -d "Permanently disable script previews in menus"
+complete -c carch -n '__fish_carch_no_subcommand' -l no-preview -d "Run without displaying script previews in menus"
 complete -c carch -n '__fish_carch_no_subcommand' -l log -d "Enable logging for the current session only"
-complete -c carch -n '__fish_carch_no_subcommand' -l enable-logging -d "Permanently enable logging in the configuration file"
-complete -c carch -n '__fish_carch_no_subcommand' -s s -d "Search for scripts by keyword"
 complete -c carch -n '__fish_carch_no_subcommand' -l check-update -d "Check if a new version of Carch is available"
 complete -c carch -n '__fish_carch_no_subcommand' -l update -d "Update Carch using the latest script"
 complete -c carch -n '__fish_carch_no_subcommand' -l uninstall -d "Uninstall Carch and remove all associated files"
