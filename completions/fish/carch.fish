417b36b797f892067868268356aeb65e6130ee33
Hari Chalise <ingoprivate@gmail.com>
2025-04-12 03:10:50 +0545

    remove: unused commands

--- completions/fish/carch.fish
+++ completions/fish/carch.fish
@@ -1,7 +1,7 @@
 function __fish_carch_no_subcommand
     for i in (commandline -opc)
         switch $i
-            case --help -h --version -v --gen-config -c --run-script -r --list-scripts -l --no-preview --log --check-update --update --uninstall
+            case --help -h --version -v --list-scripts -l --no-preview --log --check-update --update --uninstall
                 return 1
         end
     end
@@ -12,10 +12,6 @@ complete -c carch -n '__fish_carch_no_subcommand' -l help -d "Show the help mess
 complete -c carch -n '__fish_carch_no_subcommand' -s h -d "Show the help message"
 complete -c carch -n '__fish_carch_no_subcommand' -l version -d "Show the program version"
 complete -c carch -n '__fish_carch_no_subcommand' -s v -d "Show the program version"
-complete -c carch -n '__fish_carch_no_subcommand' -l gen-config -d "Generate a default configuration file"
-complete -c carch -n '__fish_carch_no_subcommand' -s c -d "Use the default configuration file"
-complete -c carch -n '__fish_carch_no_subcommand' -l run-script -d "Run a specific script from the script directory" -r
-complete -c carch -n '__fish_carch_no_subcommand' -s r -d "Run a specific script from the script directory" -r
 complete -c carch -n '__fish_carch_no_subcommand' -l list-scripts -d "List all available scripts"
 complete -c carch -n '__fish_carch_no_subcommand' -s l -d "List all available scripts"
 complete -c carch -n '__fish_carch_no_subcommand' -l no-preview -d "Run without displaying script previews in menus"
