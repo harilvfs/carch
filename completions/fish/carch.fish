d667586898bc9002a5ecef6e1bc3bbc509dc9c07
harilvfs <ingoprivate@gmail.com>
2025-04-22 22:17:06 +0545

    remove: old command

--- completions/fish/carch.fish
+++ completions/fish/carch.fish
@@ -1,7 +1,7 @@
 function __fish_carch_no_subcommand
     for i in (commandline -opc)
         switch $i
-            case --help -h --version -v --list-scripts -l --no-preview --log --check-update --update --uninstall
+            case --help -h --version -v --list-scripts -l --no-preview --log --update --uninstall
                 return 1
         end
     end
@@ -16,7 +16,6 @@ complete -c carch -n '__fish_carch_no_subcommand' -l list-scripts -d "List all a
 complete -c carch -n '__fish_carch_no_subcommand' -s l -d "List all available scripts"
 complete -c carch -n '__fish_carch_no_subcommand' -l no-preview -d "Run without displaying script previews in menus"
 complete -c carch -n '__fish_carch_no_subcommand' -l log -d "Enable logging for the current session only"
-complete -c carch -n '__fish_carch_no_subcommand' -l check-update -d "Check if a new version of Carch is available"
 complete -c carch -n '__fish_carch_no_subcommand' -l update -d "Update Carch using the latest script"
 complete -c carch -n '__fish_carch_no_subcommand' -l uninstall -d "Uninstall Carch and remove all associated files"
 
