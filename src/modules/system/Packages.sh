fd1784e526cc04efb0d30d6963d81b45b8e85c94
Hari Chalise <ingoprivate@gmail.com>
2025-05-10 23:25:55 +0545

    feat: add Electrum Bitcoin wallet install script

--- src/modules/system/Packages.sh
+++ src/modules/system/Packages.sh
@@ -1926,10 +1926,64 @@ install_virtualization() {
     done
 }
 
+install_crypto_tools() {
+    detect_distro
+    distro=$?
+
+    if [[ $distro -eq 0 ]]; then
+        install_aur_helper
+        pkg_manager_aur="$AUR_HELPER -S --noconfirm"
+        get_version() { pacman -Qi "$1" | grep Version | awk '{print $3}'; }
+    elif [[ $distro -eq 1 ]]; then
+        pkg_manager="sudo dnf install -y"
+        get_version() { rpm -q "$1"; }
+    else
+        echo -e "${RED}:: Unsupported distribution. Exiting.${RESET}"
+        return
+    fi
+
+    while true; do
+        clear
+
+        options=("Electrum" "Back to Main Menu")
+        mapfile -t selected < <(printf "%s\n" "${options[@]}" | fzf ${FZF_COMMON} \
+                                                    --height=40% \
+                                                    --prompt="Choose options (TAB to select multiple): " \
+                                                    --header="Package Selection" \
+                                                    --pointer="➤" \
+                                                    --multi \
+                                                    --color='fg:white,fg+:blue,bg+:black,pointer:blue')
+
+        if printf '%s\n' "${selected[@]}" | grep -q "Back to Main Menu" || [[ ${#selected[@]} -eq 0 ]]; then
+            return
+        fi
+
+        for selection in "${selected[@]}"; do
+            case $selection in
+            "Electrum")
+                clear
+                if [[ $distro -eq 0 ]]; then
+                    $pkg_manager_aur electrum
+                    version=$(get_version electrum)
+                else
+                    $pkg_manager electrum
+                    version=$(get_version electrum)
+                fi
+                echo "Electrum installed successfully! Version: $version"
+                ;;
+
+            esac
+        done
+
+        echo "All selected Crypto tools have been installed."
+        read -rp "Press Enter to continue..."
+    done
+}
+
 while true; do
     clear
 
-    options=("Android Tools" "Browsers" "Communication Apps" "Development Tools" "Editing Tools" "File Managers" "Gaming" "GitHub" "Multimedia" "Music Apps" "Productivity Apps" "Streaming Tools" "Terminals" "Text Editors" "Thunar Preview" "Virtualization" "Exit")
+    options=("Android Tools" "Browsers" "Communication Apps" "Development Tools" "Editing Tools" "File Managers" "Gaming" "GitHub" "Multimedia" "Music Apps" "Productivity Apps" "Streaming Tools" "Terminals" "Text Editors" "Thunar Preview" "Virtualization" "Crypto Tools" "Exit")
     selected=$(printf "%s\n" "${options[@]}" | fzf ${FZF_COMMON} \
                                                         --height=70% \
                                                         --prompt="Choose an option: " \
@@ -1954,6 +2008,7 @@ while true; do
         "Text Editors") install_texteditor ;;
         "Thunar Preview") install_thunarpreview ;;
         "Virtualization") install_virtualization ;;
+        "Crypto Tools") install_crypto_tools ;;
         "Exit") exit ;;
     esac
 done
