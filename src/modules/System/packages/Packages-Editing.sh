9152cb37a2f6e88cb843c0b8d1c1c949a8fcd10f
Hari Chalise <ingoprivate@gmail.com>
2025-05-28 23:26:00 +0545

    refactor: split package functions into sourced scripts (#465)
    * refactor: split package functions into sourced scripts
    
    * forget to put shell bang

---
+++ src/modules/System/packages/Packages-Editing.sh
@@ -0,0 +1,126 @@
+#!/usr/bin/env bash
+
+install_editing() {
+    detect_distro
+    distro=$?
+
+    if [[ $distro -eq 0 ]]; then
+        pkg_manager="sudo pacman -S --noconfirm"
+    elif [[ $distro -eq 1 ]]; then
+        pkg_manager="sudo dnf install -y"
+    else
+        echo -e "${RED}:: Unsupported system. Exiting.${RESET}"
+        return
+    fi
+
+    while true; do
+        clear
+
+        options=("GIMP (Image)" "Kdenlive (Videos)" "Krita" "Blender" "Inkscape" "Audacity" "DaVinci Resolve" "Back to Main Menu")
+        mapfile -t selected < <(printf "%s\n" "${options[@]}" | fzf ${FZF_COMMON} \
+                                                    --height=40% \
+                                                    --prompt="Choose options (TAB to select multiple): " \
+                                                    --header="Package Selection" \
+                                                    --pointer="➤" \
+                                                    --multi \
+                                                    --color='fg:white,fg+:blue,bg+:black,pointer:blue')
+
+        if printf '%s\n' "${selected[@]}" | grep -q "Back to Main Menu" || [[ ${#selected[@]} -eq 0 ]]; then
+            return
+        fi
+
+        for selection in "${selected[@]}"; do
+            case $selection in
+            "GIMP (Image)")
+                clear
+                if [[ $distro -eq 0 ]]; then
+                    $pkg_manager gimp
+                    version=$(pacman -Qi gimp | grep Version | awk '{print $3}')
+                else
+                    $pkg_manager gimp
+                    version=$(rpm -q gimp)
+                fi
+                echo "GIMP installed successfully! Version: $version"
+                ;;
+
+            "Kdenlive (Videos)")
+                clear
+                if [[ $distro -eq 0 ]]; then
+                    $pkg_manager kdenlive
+                    version=$(pacman -Qi kdenlive | grep Version | awk '{print $3}')
+                    echo "Kdenlive installed successfully! Version: $version"
+                else
+                    $pkg_manager kdenlive
+                    version=$(rpm -q kdenlive)
+                    echo "Kdenlive installed successfully! Version: $version"
+                fi
+                ;;
+
+            "Krita")
+                clear
+                if [[ $distro -eq 0 ]]; then
+                    $pkg_manager_aur krita
+                    version=$(get_version krita)
+                else
+                    $pkg_manager krita
+                    version=$(get_version krita)
+                fi
+                echo "Krita installed successfully! Version: $version"
+                ;;
+
+            "Blender")
+                clear
+                if [[ $distro -eq 0 ]]; then
+                    $pkg_manager_aur blender
+                    version=$(get_version blender)
+                else
+                    $pkg_manager blender
+                    version=$(get_version blender)
+                fi
+                echo "Blender installed successfully! Version: $version"
+                ;;
+
+            "Inkscape")
+                clear
+                if [[ $distro -eq 0 ]]; then
+                    $pkg_manager_aur inkscape
+                    version=$(get_version inkscape)
+                else
+                    $pkg_manager inkscape
+                    version=$(get_version inkscape)
+                fi
+                echo "Inkscape installed successfully! Version: $version"
+                ;;
+
+            "Audacity")
+                clear
+                if [[ $distro -eq 0 ]]; then
+                    $pkg_manager_aur audacity
+                    version=$(get_version audacity)
+                else
+                    $pkg_manager audacity
+                    version=$(get_version audacity)
+                fi
+                echo "Audacity installed successfully! Version: $version"
+                ;;
+
+            "DaVinci Resolve")
+                clear
+                if [[ $distro -eq 0 ]]; then
+                    $pkg_manager_aur davinci-resolve
+                    version=$(get_version davinci-resolve)
+                else
+                    echo "DaVinci Resolve is not directly available in Fedora repositories."
+                    echo "Download from: [Blackmagic Design Website](https://www.blackmagicdesign.com/products/davinciresolve/)"
+                    version="(Manual installation required)"
+                fi
+                echo "DaVinci Resolve installation completed! Version: $version"
+                ;;
+
+            esac
+        done
+
+        echo "All selected Editing tools have been installed."
+        read -rp "Press Enter to continue..."
+    done
+}
---
+++ src/modules/System/packages/Packages-Productivity.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+
+install_productivity() {
+    detect_distro
+    distro=$?
+
+    if [[ $distro -eq 0 ]]; then
+        install_aur_helper
+        pkg_manager_aur="$AUR_HELPER -S --noconfirm"
+        pkg_manager_pacman="sudo pacman -S --noconfirm"
+        get_version() { pacman -Qi "$1" | grep Version | awk '{print $3}'; }
+    elif [[ $distro -eq 1 ]]; then
+        install_flatpak
+        pkg_manager="sudo dnf install -y"
+        flatpak_cmd="flatpak install -y --noninteractive flathub"
+        get_version() { rpm -q "$1"; }
+    else
+        echo -e "${RED}:: Unsupported distribution. Exiting.${RESET}"
+        return
+    fi
+
+    while true; do
+        clear
+
+        options=("LibreOffice" "OnlyOffice" "Obsidian" "Joplin" "Calibre" "Back to Main Menu")
+        mapfile -t selected < <(printf "%s\n" "${options[@]}" | fzf ${FZF_COMMON} \
+                                                    --height=40% \
+                                                    --prompt="Choose options (TAB to select multiple): " \
+                                                    --header="Package Selection" \
+                                                    --pointer="➤" \
+                                                    --multi \
+                                                    --color='fg:white,fg+:blue,bg+:black,pointer:blue')
+
+        if printf '%s\n' "${selected[@]}" | grep -q "Back to Main Menu" || [[ ${#selected[@]} -eq 0 ]]; then
+            return
+        fi
+
+        for selection in "${selected[@]}"; do
+            case $selection in
+            "LibreOffice")
+                clear
+                if [[ $distro -eq 0 ]]; then
+                    $pkg_manager_pacman libreoffice-fresh
+                    version=$(get_version libreoffice-fresh)
+                else
+                    $pkg_manager libreoffice
+                    version=$(get_version libreoffice)
+                fi
+                echo "LibreOffice installed successfully! Version: $version"
+                ;;
+
+            "OnlyOffice")
+                clear
+                if [[ $distro -eq 0 ]]; then
+                    $pkg_manager_aur onlyoffice-bin
+                    version=$(get_version onlyoffice-bin)
+                else
+                    $flatpak_cmd org.onlyoffice.desktopeditors
+                    version="(Flatpak version installed)"
+                fi
+                echo "OnlyOffice installed successfully! Version: $version"
+                ;;
+
+            "Obsidian")
+                clear
+                if [[ $distro -eq 0 ]]; then
+                    $pkg_manager_aur obsidian
+                    version=$(get_version obsidian)
+                else
+                    $flatpak_cmd md.obsidian.Obsidian
+                    version="(Flatpak version installed)"
+                fi
+                echo "Obsidian installed successfully! Version: $version"
+                ;;
+
+            "Joplin")
+                clear
+                if [[ $distro -eq 0 ]]; then
+                    $pkg_manager_aur joplin-desktop
+                    version=$(get_version joplin-desktop)
+                else
+                    $flatpak_cmd net.cozic.joplin_desktop
+                    version="(Flatpak version installed)"
+                fi
+                echo "Joplin installed successfully! Version: $version"
+                ;;
+
+            "Calibre")
+                clear
+                if [[ $distro -eq 0 ]]; then
+                    $pkg_manager_pacman calibre
+                    version=$(get_version calibre)
+                else
+                    $pkg_manager calibre
+                    version=$(get_version calibre)
+                fi
+                echo "Calibre installed successfully! Version: $version"
+                ;;
+
+            esac
+        done
+
+        echo "All selected Productivity Apps have been installed."
+        read -rp "Press Enter to continue..."
+    done
+}
