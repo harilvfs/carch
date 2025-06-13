253d10230f118feb569adbd77c52d8544eb51bfe
Hari Chalise <ingoprivate@gmail.com>
2025-06-14 02:13:10 +0545

    feat(installer): rewrite install script in Go (#500)
    * feat(installer): rewrite install script in Go
    
    * fix linting issue
    
    * will test in production after merge
    
    * manual run

---
+++ scripts/install/utils.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/briandowns/spinner"
+)
+
+func fileExists(path string) bool {
+	_, err := os.Stat(path)
+	return !os.IsNotExist(err)
+}
+
+func dirExists(path string) bool {
+	info, err := os.Stat(path)
+	return !os.IsNotExist(err) && info.IsDir()
+}
+
+func createDir(path string) error {
+	return runCommand("sudo", "mkdir", "-p", path)
+}
+
+func makeExecutable(path string) error {
+	return os.Chmod(path, 0755)
+}
+
+func sudoMoveFile(src, dest string) error {
+	return runCommand("sudo", "mv", src, dest)
+}
+
+func commandExists(command string) bool {
+	_, err := exec.LookPath(command)
+	return err == nil
+}
+
+func runCommand(name string, args ...string) error {
+	cmd := exec.Command(name, args...)
+	return cmd.Run()
+}
+
+func downloadFile(url string) (string, error) {
+	tmpFile, err := os.CreateTemp("", "carch-download-*")
+	if err != nil {
+		return "", fmt.Errorf("failed to create temp file: %v", err)
+	}
+	defer func() {
+		_ = tmpFile.Close()
+	}()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		_ = os.Remove(tmpFile.Name())
+		return "", fmt.Errorf("failed to download file: %v", err)
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		_ = os.Remove(tmpFile.Name())
+		return "", fmt.Errorf("download failed with status: %s", resp.Status)
+	}
+
+	_, err = io.Copy(tmpFile, resp.Body)
+	if err != nil {
+		_ = os.Remove(tmpFile.Name())
+		return "", fmt.Errorf("failed to save file: %v", err)
+	}
+
+	return tmpFile.Name(), nil
+}
+
+func installDependencies(deps []string) error {
+	s := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
+	s.Suffix = fmt.Sprintf(" Installing dependencies: %s", strings.Join(deps, ", "))
+	s.Start()
+	defer s.Stop()
+
+	var err error
+	if commandExists("pacman") {
+		args := append([]string{"-Sy", "--noconfirm"}, deps...)
+		err = runCommand("sudo", append([]string{"pacman"}, args...)...)
+	} else if commandExists("dnf") {
+		args := append([]string{"install", "-y"}, deps...)
+		err = runCommand("sudo", append([]string{"dnf"}, args...)...)
+	} else if commandExists("yum") {
+		args := append([]string{"install", "-y"}, deps...)
+		err = runCommand("sudo", append([]string{"yum"}, args...)...)
+	} else {
+		s.Stop()
+		return fmt.Errorf("unsupported package manager. Please install the following dependencies manually: %s", strings.Join(deps, ", "))
+	}
+
+	time.Sleep(500 * time.Millisecond)
+	return err
+}
