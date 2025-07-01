ad517c751fdc616c4a80e4d346c4ea3a812582cd
Hari Chalise <ingoprivate@gmail.com>
2025-07-02 02:08:56 +0545

    refactor(script): remove custom spinner

--- scripts/stable.sh
+++ scripts/stable.sh
@@ -10,7 +10,7 @@ check_dependency() {
         elif command -v dnf > /dev/null 2>&1; then
             sudo dnf install -y "$1"
         else
-            printf '%sERROR: Package manager not supported%s\n' "$red" "$rc"
+            printf '%bERROR: Package manager not supported%b\n' "$red" "$rc"
             exit 1
         fi
     fi
@@ -24,7 +24,7 @@ check() {
     exit_code=$1
     message=$2
     if [ "$exit_code" -ne 0 ]; then
-        printf '%sERROR: %s%s\n' "$red" "$message" "$rc"
+        printf '%bERROR: %s%b\n' "$red" "$message" "$rc"
         exit 1
     fi
 }
@@ -44,29 +44,21 @@ getUrl() {
     esac
 }
 
-spinner() {
-    local pid=$1
-    local spin="⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"
-    local i=0
-
-    while kill -0 "$pid" 2> /dev/null; do
-        printf "\r%s" "${spin:i++%${#spin}:1}"
-        sleep 0.1
-    done
-    printf "\r✔\n"
-}
+TIMEOUT=10
 
 findArch
 temp_file=$(mktemp)
 check $? "Creating the temporary file"
 
-curl -fsL "$(getUrl)" -o "$temp_file" &
-spinner $!
-
+url="$(getUrl)"
+curl -L --progress-bar --connect-timeout "$TIMEOUT" --max-time 120 "$url" -o "$temp_file"
 check $? "Downloading carch"
+
 chmod +x "$temp_file"
 check $? "Making carch executable"
+
 "$temp_file" "$@"
 check $? "Executing carch"
+
 rm -f "$temp_file"
 check $? "Deleting the temporary file"
