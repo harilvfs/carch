1f737f8d31694568bd627a03d271541f950b766d
Hari Chalise <ingoprivate@gmail.com>
2025-07-15 01:49:03 +0545

    editing one done

--- src/modules/System/packages/Packages-Editing.sh
+++ src/modules/System/packages/Packages-Editing.sh
@@ -6,8 +6,15 @@ install_editing() {
 
     if [[ $distro -eq 0 ]]; then
         pkg_manager="sudo pacman -S --noconfirm"
+        get_version() { pacman -Qi "$1" | grep Version | awk '{print $3}'; }
     elif [[ $distro -eq 1 ]]; then
         pkg_manager="sudo dnf install -y"
+        get_version() { rpm -q "$1"; }
+    elif [[ $distro -eq 2 ]]; then
+        install_flatpak
+        pkg_manager="sudo zypper install -y"
+        flatpak_cmd="flatpak install -y --noninteractive flathub"
+        get_version() { rpm -q "$1"; }
     else
         echo -e "${RED}:: Unsupported system. Exiting.${NC}"
         return
@@ -33,27 +40,16 @@ install_editing() {
             case $selection in
                 "GIMP (Image)")
                     clear
-                    if [[ $distro -eq 0 ]]; then
-                        $pkg_manager gimp
-                        version=$(pacman -Qi gimp | grep Version | awk '{print $3}')
-                    else
-                        $pkg_manager gimp
-                        version=$(rpm -q gimp)
-                    fi
+                    $pkg_manager gimp
+                    version=$(get_version gimp)
                     echo "GIMP installed successfully! Version: $version"
                     ;;
 
                 "Kdenlive (Videos)")
                     clear
-                    if [[ $distro -eq 0 ]]; then
-                        $pkg_manager kdenlive
-                        version=$(pacman -Qi kdenlive | grep Version | awk '{print $3}')
-                        echo "Kdenlive installed successfully! Version: $version"
-                    else
-                        $pkg_manager kdenlive
-                        version=$(rpm -q kdenlive)
-                        echo "Kdenlive installed successfully! Version: $version"
-                    fi
+                    $pkg_manager kdenlive
+                    version=$(get_version kdenlive)
+                    echo "Kdenlive installed successfully! Version: $version"
                     ;;
 
                 "Krita")
@@ -73,9 +69,12 @@ install_editing() {
                     if [[ $distro -eq 0 ]]; then
                         $pkg_manager_aur blender
                         version=$(get_version blender)
-                    else
+                    elif [[ $distro -eq 1 ]]; then
                         $pkg_manager blender
                         version=$(get_version blender)
+                    else
+                        $flatpak_cmd org.blender.Blender
+                        version="(Flatpak version installed)"
                     fi
                     echo "Blender installed successfully! Version: $version"
                     ;;
@@ -85,9 +84,12 @@ install_editing() {
                     if [[ $distro -eq 0 ]]; then
                         $pkg_manager_aur inkscape
                         version=$(get_version inkscape)
-                    else
+                    elif [[ $distro -eq 1 ]]; then
                         $pkg_manager inkscape
                         version=$(get_version inkscape)
+                    else
+                        $flatpak_cmd org.inkscape.Inkscape
+                        version="(Flatpak version installed)"
                     fi
                     echo "Inkscape installed successfully! Version: $version"
                     ;;
@@ -110,7 +112,7 @@ install_editing() {
                         $pkg_manager_aur davinci-resolve
                         version=$(get_version davinci-resolve)
                     else
-                        echo "DaVinci Resolve is not directly available in Fedora repositories."
+                        echo "DaVinci Resolve is not directly available in official repositories."
                         echo "Download from: [Blackmagic Design Website](https://www.blackmagicdesign.com/products/davinciresolve/)"
                         version="(Manual installation required)"
                     fi
