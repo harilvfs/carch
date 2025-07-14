c7ee65970901fa9e78324502bcbcfd312373e042
Hari Chalise <ingoprivate@gmail.com>
2025-07-15 01:30:38 +0545

    crypto done

--- src/modules/System/packages/Packages-Crypto-Tools.sh
+++ src/modules/System/packages/Packages-Crypto-Tools.sh
@@ -11,6 +11,11 @@ install_crypto_tools() {
     elif [[ $distro -eq 1 ]]; then
         pkg_manager="sudo dnf install -y"
         get_version() { rpm -q "$1"; }
+    elif [[ $distro -eq 2 ]]; then
+        install_flatpak
+        pkg_manager="sudo zypper install -y"
+        flatpak_cmd="flatpak install -y --noninteractive flathub"
+        get_version() { rpm -q "$1"; }
     else
         echo -e "${RED}:: Unsupported distribution. Exiting.${NC}"
         return
@@ -39,13 +44,17 @@ install_crypto_tools() {
                     if [[ $distro -eq 0 ]]; then
                         $pkg_manager_aur electrum
                         version=$(get_version electrum)
-                    else
+                        echo "Electrum installed successfully! Version: $version"
+                    elif [[ $distro -eq 1 ]]; then
                         $pkg_manager electrum
                         version=$(get_version electrum)
+                        echo "Electrum installed successfully! Version: $version"
+                    else
+                        $flatpak_cmd org.electrum.electrum
+                        version="(Flatpak version installed)"
+                        echo "Electrum installed successfully! Version: $version"
                     fi
-                    echo "Electrum installed successfully! Version: $version"
                     ;;
-
             esac
         done
 
