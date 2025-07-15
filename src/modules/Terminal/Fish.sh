77d62e579335f78add651cf25fff95b6538b4066
Hari Chalise <ingoprivate@gmail.com>
2025-07-15 22:36:56 +0545

    fix upper case

--- src/modules/Terminal/Fish.sh
+++ src/modules/Terminal/Fish.sh
@@ -131,7 +131,7 @@ if ! command -v fzf &> /dev/null; then
     echo -e "${YELLOW}Please install fzf before running this script:${NC}"
     echo -e "${CYAN}  • Fedora: ${NC}sudo dnf install fzf"
     echo -e "${CYAN}  • Arch Linux: ${NC}sudo pacman -S fzf"
-    echo -e "${CYAN}  • openSuse: ${NC}sudo zypper install fzf"
+    echo -e "${CYAN}  • openSUSE: ${NC}sudo zypper install fzf"
     exit 1
 fi
 
