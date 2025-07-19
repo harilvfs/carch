ff170b2afc74eaeca9efcaacf2b7cea76c69dd88
Hari Chalise <ingoprivate@gmail.com>
2025-07-19 23:02:25 +0545

    chore(commands): add missing commands entry

--- completions/fish/carch.fish
+++ completions/fish/carch.fish
@@ -1,7 +1,7 @@
 function __fish_carch_no_subcommand
     for i in (commandline -opc)
         switch $i
-            case --help -h --version -v --log --check-update --update --uninstall
+            case --help -h --version -v --log --no-cleanup check-update update uninstall
                 return 1
         end
     end
@@ -13,7 +13,7 @@ complete -c carch -n '__fish_carch_no_subcommand' -s h -d "Show the help message
 complete -c carch -n '__fish_carch_no_subcommand' -l version -d "Show the program version"
 complete -c carch -n '__fish_carch_no_subcommand' -s v -d "Show the program version"
 complete -c carch -n '__fish_carch_no_subcommand' -l log -d "Enable logging for the current session only"
-complete -c carch -n '__fish_carch_no_subcommand' -l check-update -d "Check if a new version of Carch is available"
-complete -c carch -n '__fish_carch_no_subcommand' -l update -d "Interactively update Carch"
-complete -c carch -n '__fish_carch_no_subcommand' -l uninstall -d "Interactively uninstall Carch"
-
+complete -c carch -n '__fish_carch_no_subcommand' -l no-cleanup -d "Disable cache cleanup"
+complete -c carch -n '__fish_carch_no_subcommand' -a 'check-update' -d "Check if a new version of Carch is available"
+complete -c carch -n '__fish_carch_no_subcommand' -a 'update' -d "Interactively update Carch"
+complete -c carch -n '__fish_carch_no_subcommand' -a 'uninstall' -d "Interactively uninstall Carch"
