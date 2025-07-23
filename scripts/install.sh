790821f9b7e5b44d44b8dc274b71fffa02a43368
Hari Chalise <ingoprivate@gmail.com>
2025-07-24 00:58:55 +0545

    feat(install): add output text's

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,10 @@
 
 set -eu
 
+YELLOW='\033[1;33m'
+GREEN='\033[1;32m'
+NC='\033[0m'
+
 detect_distro() {
     if command -v pacman > /dev/null 2>&1; then
         echo "arch"
@@ -15,35 +19,40 @@ detect_distro() {
 }
 
 install_arch() {
+    printf "${GREEN}==> ${NC}Cloning pkgbuild repo\n"
     rm -rf ~/pkgs
-    git clone https://github.com/carch-org/pkgs ~/pkgs
+    git clone https://github.com/carch-org/pkgs ~/pkgs > /dev/null 2>&1
+
     cd ~/pkgs/carch-bin || exit 1
     makepkg -si
 }
 
 install_fedora() {
+    printf "${YELLOW}:: ${NC}downloading carch rpm\n"
     rpm_url=$(curl -sL https://api.github.com/repos/harilvfs/carch/releases/latest | grep browser_download_url | grep '\.rpm"' | cut -d '"' -f 4)
     if [ -z "$rpm_url" ]; then
         printf "Error: Could not find RPM package URL\n"
         exit 1
     fi
-    printf "%s\n" "$rpm_url" | tee /tmp/carch.rpm
-    sudo dnf install -y "$rpm_url"
+    curl -sL "$rpm_url" -o /tmp/carch.rpm > /dev/null 2>&1
+
+    sudo dnf install -y /tmp/carch.rpm
 }
 
 install_opensuse() {
+    printf "${YELLOW}:: ${NC}downloading carch rpm\n"
     rpm_url=$(curl -sL https://api.github.com/repos/harilvfs/carch/releases/latest | grep browser_download_url | grep '\.rpm"' | cut -d '"' -f 4)
     if [ -z "$rpm_url" ]; then
         printf "Error: Could not find RPM package URL\n"
         exit 1
     fi
-    printf "%s\n" "$rpm_url" | tee /tmp/carch.rpm
-    sudo zypper install -y "$rpm_url"
+    curl -sL "$rpm_url" -o /tmp/carch.rpm > /dev/null 2>&1
+
+    sudo zypper install -y /tmp/carch.rpm
 }
 
 main() {
     distro=$(detect_distro)
-
     case "$distro" in
         arch)
             install_arch
