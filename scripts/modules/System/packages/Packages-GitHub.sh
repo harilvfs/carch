d6c9827933c695e5bf0e8c2329b14de141832fc7
Hari Chalise <ingoprivate@gmail.com>
2025-08-03 21:24:49 +0545

    using flatpak to install github desktop

--- scripts/modules/System/packages/Packages-GitHub.sh
+++ scripts/modules/System/packages/Packages-GitHub.sh
@@ -19,58 +19,7 @@ install_github() {
 
             "GitHub Desktop")
                 clear
-                case "$DISTRO" in
-                    "Arch")
-                        install_package "github-desktop-bin" "io.github.shiftey.Desktop"
-                        ;;
-                    "Fedora" | "openSUSE")
-                        print_message "$GREEN" "Downloading GitHub Desktop from latest release..."
-
-                        if ! command -v curl &> /dev/null; then
-                            print_message "$GREEN" "Installing curl..."
-                            install_package "curl" ""
-                        fi
-
-                        if ! command -v wget &> /dev/null; then
-                            print_message "$GREEN" "Installing wget..."
-                            install_package "wget" ""
-                        fi
-
-                        latest_release=$(curl -s https://api.github.com/repos/shiftkey/desktop/releases/latest)
-                        if [[ -z "$latest_release" ]]; then
-                            print_message "$RED" "Failed to fetch latest release info. Exiting."
-                            continue
-                        fi
-
-                        rpm_url=$(echo "$latest_release" | grep -o 'https://github.com/shiftkey/desktop/releases/download/[^\\"]*GitHubDesktop-linux-x86_64-[^\\"]*\.rpm' | head -1)
-
-                        if [[ -z "$rpm_url" ]]; then
-                            print_message "$RED" "Failed to find RPM download URL. Exiting."
-                            continue
-                        fi
-
-                        print_message "$GREEN" "Found RPM URL: $rpm_url"
-
-                        tmp_dir=$(mktemp -d)
-                        cd "$tmp_dir" || exit 1
-
-                        print_message "$GREEN" "Downloading GitHub Desktop RPM..."
-                        if wget "$rpm_url"; then
-                            rpm_file=$(basename "$rpm_url")
-
-                            print_message "$GREEN" "Installing GitHub Desktop..."
-                            case "$DISTRO" in
-                                "Fedora") sudo dnf install -y "./$rpm_file" ;;
-                                "openSUSE") sudo zypper install -y --allow-unsigned-rpm "./$rpm_file" ;;
-                            esac
-                        else
-                            print_message "$RED" "Failed to download GitHub Desktop RPM."
-                        fi
-
-                        cd /
-                        rm -rf "$tmp_dir"
-                        ;;
-                esac
+                install_package "github-desktop-bin" "io.github.shiftey.Desktop"
                 ;;
 
             "GitHub CLI")
