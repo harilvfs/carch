46795c92428ee54147c07804f5c93e528b94b229
Hari Chalise <ingoprivate@gmail.com>
2026-05-16 00:23:17 +0545

    feat(termux): add shell setup script

--- carch-core/src/modules/detect-distro.sh
+++ carch-core/src/modules/detect-distro.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 detect_distro() {
-    if [ -x "$(command -v pacman)" ]; then
+    if [ -n "$TERMUX_VERSION" ] || [ -d "/data/data/com.termux" ] || [ "$(uname -o 2> /dev/null)" = "Android" ]; then
+        DISTRO="Termux"
+    elif [ -x "$(command -v pacman)" ]; then
         DISTRO="Arch"
     elif [ -x "$(command -v dnf)" ]; then
         DISTRO="Fedora"
