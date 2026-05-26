282a213c24020cc3421490a7de3ecf7fcf5d65c1
Hari Chalise <ingoprivate@gmail.com>
2026-05-26 23:11:30 +0545

    chore(script): remove direct distro detect for termux for all scripts

--- carch-core/src/modules/detect-distro.sh
+++ carch-core/src/modules/detect-distro.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 detect_distro() {
-    if [ -n "$TERMUX_VERSION" ] || [ -d "/data/data/com.termux" ] || [ "$(uname -o 2> /dev/null)" = "Android" ]; then
-        DISTRO="Termux"
-    elif [ -x "$(command -v pacman)" ]; then
+    if [ -x "$(command -v pacman)" ]; then
         DISTRO="Arch"
     elif [ -x "$(command -v dnf)" ]; then
         DISTRO="Fedora"
